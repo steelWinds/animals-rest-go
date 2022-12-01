815292b035b88bdcfd1f8efa3907e79f5db2d4bb
steelWinds <kirillsurov0@gmail.com>
2022-12-01 20:36:32 +0900

    update(app): add air for go live-reloading

--- app/main.go
+++ app/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 
 	"github.com/steelWinds/animals-rest-go/handlers"
 )
 
 func main() {
-	fmt.Println("I'm first go-docker app!")
-
 	router := gin.Default()
 
 	router.GET("/animals", handlers.GetAnimals)
